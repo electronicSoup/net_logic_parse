8ce6b6885f0d0903dcc1662f8db0b1a5f5894b37
John Whitmore <johnfwhitmore@gmail.com>
2018-12-10 18:20:12 +0000

    First step to populate array of components on diagram
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- parse.py
+++ parse.py
@@ -9,7 +9,8 @@ class Component:
         self.ref = ref
         self.node_type = node_type
         self.address = address
-        child_nodes = []
+        self.child_nodes = []
+        self.logic = None
 
 components = []
 
@@ -27,16 +28,27 @@ for component in nlst.components:
     components.append(Component(component.ref.val, component.part.val, component.value.val))
 
 for net in nlst.nets:
-    nodes = []
+    net_nodes = []
+    input_node = None
+    input_ref = None
+
     for node in net.nodes:
-        nodes.append(node.ref.val)
+        net_nodes.append(node.ref.val)
     print(net.name.val)
-    for ref in nodes:
+
+    # Find the input node on the current net
+    for ref in net_nodes:
         index = find_component(ref)
         if index is not None:
             print("Found {}".format(components[index].ref))
+            if components[index].node_type == 'bool_input_431':
+                input_node = index
+                input_ref = components[index].ref
         else:
             print("NOT FOUND!")
+
+    # Now have the input node so add all other nodes on the net to the Input nodes
+    # list of effected outputs.
             
 
 #
@@ -46,5 +58,16 @@ for net in nlst.nets:
 output_file = open("/home/john/Private/es/programming/mcp/CinnamonBun/Firmware/CAN_Node.X/src/application/Controller/network.h","w")
 
 output_file.write('#define NUM_NET_NODES %d\n' % (len(nlst.components),))
-output_file.write('static struct net_node nnodes[NUM_NET_NODES];\n')
+output_file.write('static struct net_node nnodes[NUM_NET_NODES] = {\n')
+
+for component in components:
+    output_file.write('\t{\n')
+    output_file.write('\t\t.type = ')
+    if component.node_type == 'bool_input_431':
+        output_file.write('b431_in\n')
+    if component.node_type == 'bool_output_431':
+        output_file.write('b431_out\n')
+    output_file.write('\t},\n')
+output_file.write('};\n')
+
 
