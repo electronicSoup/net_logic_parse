6f491b649382c31fc32b0d4f040bb06607758315
John Whitmore <johnfwhitmore@gmail.com>
2018-12-11 17:42:26 +0000

    Added process to add the array of effected nodes
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

--- parse.py
+++ parse.py
@@ -9,7 +9,7 @@ class Component:
         self.ref = ref
         self.node_type = node_type
         self.address = address
-        self.child_nodes = []
+        self.effected_nodes = []
         self.logic = None
 
 components = []
@@ -28,36 +28,42 @@ for component in nlst.components:
     components.append(Component(component.ref.val, component.part.val, component.value.val))
 
 for net in nlst.nets:
-    net_nodes = []
+    in431_nodes = []
+    out431_nodes = []
     input_node = None
     input_ref = None
 
     for node in net.nodes:
-        net_nodes.append(node.ref.val)
-    print(net.name.val)
+        index = find_component(node.ref.val)
 
-    # Find the input node on the current net
-    for ref in net_nodes:
-        index = find_component(ref)
-        if index is not None:
-            print("Found {}".format(components[index].ref))
-            if components[index].node_type == 'bool_input_431':
-                input_node = index
-                input_ref = components[index].ref
-        else:
-            print("{} NOT FOUND!".format(ref))
+        if index is None:
+            print("Error Node {} not found".format(node.ref.val))
             exit
             
+        if components[index].node_type == 'bool_input_431':
+            print("Added Input node for net {}".format(index))
+            in431_nodes.append(index)
+        if components[index].node_type == 'bool_output_431':
+            print("Added Output node for net {}".format(index))
+            out431_nodes.append(index)
 
-    # Now have the input node so add all other nodes on the net to the Input nodes
-    # list of effected outputs.
-            
+    for in_index in in431_nodes:
+        for out_index in out431_nodes:
+            components[in_index].effected_nodes.append(out_index)
 
 #
 # This is just a hard coded output file for the moment. It's picked up from an
 # MPLAB-X Project for the controller.
 #
 output_file = open("/home/john/Private/es/programming/mcp/CinnamonBun/Firmware/CAN_Node.X/src/application/Controller/network.h","w")
+
+for component in components:
+    if len(component.effected_nodes) > 0:
+        output_file.write('uint16_t {}_effected[{}] = '.format(component.ref, len(component.effected_nodes)))
+        output_file.write('{')        
+        for node in component.effected_nodes:
+            output_file.write('{},'.format(node))
+        output_file.write('};\n')
 
 output_file.write('#define NUM_NET_NODES %d\n' % (len(nlst.components),))
 output_file.write('static struct net_node nnodes[NUM_NET_NODES] = {\n')
@@ -76,9 +82,12 @@ for component in components:
     output_file.write('\t\t.address.es_bool.byte = %s,\n' % (component.address,))
 
     # Effected component list
-    output_file.write('\t\t.number_effected = 0,\n')
-    output_file.write('\t\t.effected = NULL,\n')
-
+    output_file.write('\t\t.number_effected = {},\n'.format(len(component.effected_nodes)))
+    if len(component.effected_nodes):
+        output_file.write('\t\t.effected = (uint16_t *)&{}_effected[0],\n'.format(component.ref))
+    else:
+        output_file.write('\t\t.effected = NULL,\n')
+        
     output_file.write('\t\t.logic = NULL,\n')
 
     output_file.write('\t},\n')
